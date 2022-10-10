6ebbb098a8080c6815935d7edbf7e515a604eb8d
Felipefams <augustofams@gmail.com>
2022-10-10 11:40:37 -0300

    insertion sort feito

--- SortFiles.java
+++ SortFiles.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -6,17 +7,27 @@ public class SortFiles {
 
     public static final long maxSize = 10; // numero maximo de registros que o pc aguenta por sort
 
+    public static void insertionSort(ArrayList<BankAccount> a){
+        final int n = a.size();
+        for(int i = 1; i < n; ++i){
+            BankAccount tmp = a.get(i);
+            int j = i - 1;
+            while((j >= 0) && (a.get(j).getId() > tmp.getId())){
+                a.set(j+1, a.get(j));
+                j--;
+            }
+            a.set(j+1, tmp);
+        }
+    }
+
     public static void solve(String source) {
         ArrayList<BankAccount> bar = new ArrayList<>();
 
         //enquanto i < que a capacidade maxima do pc, pegar blocos do arquivo
         for(int i = 1; i < maxSize; ++i) bar.add(Crud.searchById(source, i)); 
-        Collections.sort(bar, new Comparator<BankAccount>() {
-            public int compare(BankAccount e1, BankAccount e2){
-                return e1.getCpf().compareTo(e2.getCpf());
-            } 
-        });
-        for(var x : bar) System.out.println(x.getCpf());
+        insertionSort(bar);//como na maior parte dos casos o array vai estar quase ordenado, fica O(n) amortizado
+        
+        for(var x : bar) System.out.println(x.getId());
         // Arrays.sort(bar);
     }
     public static void main(String[] args) {
