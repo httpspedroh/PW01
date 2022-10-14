d10cee0317ceeedac75d4edd766a2524e84c3e5b
Felipefams <augustofams@gmail.com>
2022-10-14 20:39:01 -0300

    invertido feito

--- Inverted.java
+++ Inverted.java
@@ -15,7 +15,8 @@ public class Inverted {
         public T1 first;
         public T2 second;
 
-        Pair() {}
+        Pair() {
+        }
 
         Pair(T1 fi, T2 sc) {
 
@@ -23,56 +24,101 @@ public class Inverted {
             this.second = sc;
         }
 
-        public T1 getFirst() { return this.first; }
-        public T2 getSecond() { return this.second; }
+        public T1 getFirst() {
+            return this.first;
+        }
+
+        public T2 getSecond() {
+            return this.second;
+        }
     }
 
     // ------------------------------------------------------------------------- //
 
     public static void main(String[] args) throws IOException {
 
-        String output = "cityIndex.bin";
+        String outputCity = "cityIndex.bin";
+        String outputName = "nameIndex.bin";
 
-        HashMap<String, ArrayList<Long>> hst = new HashMap<>();
+        HashMap<String, ArrayList<Long>> harr = new HashMap<>();
+        HashMap<String, ArrayList<Long>> harr2 = new HashMap<>();
 
         try {
 
-            for(int i = 0; i < Order.getTotalAccounts(); ++i) {
+            //se possivel, dividir isso daqui em uma funcao @pedro!!!
+            //parte das cidades
+            for (int i = 0; i < Order.getTotalAccounts(); ++i) {
 
-                Pair<String, Long> p = getBaIndexOf(i);
+                Pair<BankAccount, Long> p = getBaIndexOf(i);
                 ArrayList<Long> tmp = new ArrayList<>();
 
-                if(p == null) continue;
+                if (p == null)
+                    continue;
 
                 tmp.add(p.second);
 
-                if(hst.get(p.first) == null) hst.put(p.first, tmp);
+                if (harr.get(p.first.getCity()) == null)
+                    harr.put(p.first.getCity(), tmp);
                 else {
 
-                    ArrayList<Long> k = hst.get(p.first);
+                    ArrayList<Long> k = harr.get(p.first.getCity());
 
                     k.add(p.second);
-                    hst.put(p.first, k);
+                    harr.put(p.first.getCity(), k);
                 }
             }
 
-            for(var x : hst.keySet()) {
-
+            for (var x : harr.keySet()) {
                 String key = x.toString();
                 System.out.print(key + " ");
-                System.out.print(hst.get(x).size() + " ");
+                System.out.print(harr.get(x).size() + " ");
 
-                for(var y : hst.get(x)) System.out.print(y + " ");
+                for (var y : harr.get(x))
+                    System.out.print(y + " ");
 
                 System.out.println();
             }
-        } 
-        catch(FileNotFoundException e) { e.printStackTrace(); }
+
+            //parte dos nomes
+            for (int i = 0; i < Order.getTotalAccounts(); ++i) {
+
+                Pair<BankAccount, Long> p = getBaIndexOf(i);
+                ArrayList<Long> tmp = new ArrayList<>();
+
+                if (p == null)
+                    continue;
+
+                tmp.add(p.second);
+
+                if (harr2.get(p.first.getName()) == null)
+                    harr2.put(p.first.getName(), tmp);
+                else {
+
+                    ArrayList<Long> k = harr2.get(p.first.getName());
+
+                    k.add(p.second);
+                    harr2.put(p.first.getName(), k);
+                }
+            }
+
+            for (var x : harr2.keySet()) {
+                String key = x.toString();
+                System.out.print(key + " ");
+                System.out.print(harr2.get(x).size() + " ");
+
+                for (var y : harr2.get(x))
+                    System.out.print(y + " ");
+
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     // ------------------------------------------------------------------------- //
 
-    public static Pair<String, Long> getBaIndexOf(int index) throws IOException {
+    public static Pair<BankAccount, Long> getBaIndexOf(int index) throws IOException {
 
         RandomAccessFile raf = new RandomAccessFile(Main.DEFAULT_FILE, "rw");
         BankAccount ba = new BankAccount();
@@ -83,13 +129,13 @@ public class Inverted {
 
         try {
 
-            while(raf.getFilePointer() < raf.length()) {
+            while (raf.getFilePointer() < raf.length()) {
 
-                if(raf.readByte() == 0) {
+                if (raf.readByte() == 0) {
 
                     validos++;
 
-                    if(validos == index + 1) {
+                    if (validos == index + 1) {
 
                         long k = raf.getFilePointer();
 
@@ -105,23 +151,26 @@ public class Inverted {
                         ba.setTransfers(raf.readInt());
 
                         int emailsCount = raf.readInt();
-                        for(int i = 0; i < emailsCount; i++) ba.addEmail(raf.readUTF());
+                        for (int i = 0; i < emailsCount; i++)
+                            ba.addEmail(raf.readUTF());
 
                         raf.close();
 
-                        Pair<String, Long> p = new Pair<>(ba.getCity(), k);
+                        Pair<BankAccount, Long> p = new Pair<>(ba, k);
                         return p;
-                    } 
-                    else raf.skipBytes(raf.readInt());
-                } 
-                else raf.skipBytes(raf.readInt());
+                    } else
+                        raf.skipBytes(raf.readInt());
+                } else
+                    raf.skipBytes(raf.readInt());
             }
 
             raf.close();
-        } 
-        catch(Exception e) { return null; }
+        } catch (Exception e) {
+            return null;
+        }
         return null;
     }
 }
 
-// ----------------------------------------------------------------------------------------------------------------- //
+// -----------------------------------------------------------------------------------------------------------------
+// //
